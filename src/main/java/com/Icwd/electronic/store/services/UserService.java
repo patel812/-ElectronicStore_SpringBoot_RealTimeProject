ce1e1b3e961d17fc3fbf610ff88cc0ede2147a88
Abhishek Patel <abhishekpatel1238@gmail.com>
2024-02-24 13:42:45 +0530

    5) Creating User Service

---
+++ src/main/java/com/Icwd/electronic/store/services/UserService.java
@@ -0,0 +1,42 @@
+package com.Icwd.electronic.store.services;
+
+
+import com.Icwd.electronic.store.dtos.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    //Create
+ //ReturnType -> MethodName  ->Object  -> ObjectType
+     UserDto     createUser   (UserDto    userDto);
+
+
+    //Update
+    UserDto  updateUser(UserDto userDto, String userId);
+
+
+    //delete (Not need a return type)
+    void deleteUser(String userId);
+
+
+    //get all users
+    List<UserDto> getAllUser();
+
+
+    //get single user by id
+    UserDto getUserById(String userId);
+
+
+    //get single user by email id
+    UserDto getUserByEmail(String email);
+
+
+    //Search user
+    List<UserDto> searchUser(String keyword);
+
+
+    //Other user specific  features
+
+
+}
