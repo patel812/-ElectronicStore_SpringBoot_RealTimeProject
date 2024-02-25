e3a71ce085ed1d5305e01c47d6ae79528f69e39b
Abhishek Patel <abhishekpatel1238@gmail.com>
2024-02-25 13:56:10 +0530

    10) Finishing User Controller Api (Create, Update, Delete, Get all, Get single, Get by email, Get by search keyword)

---
+++ src/main/java/com/Icwd/electronic/store/controllers/UserController.java
@@ -0,0 +1,102 @@
+package com.Icwd.electronic.store.controllers;
+
+import com.Icwd.electronic.store.dtos.UserDto;
+import com.Icwd.electronic.store.entities.User;
+import com.Icwd.electronic.store.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//Controller take request
+@RestController
+
+//handle Url Request
+@RequestMapping("/users")
+public class UserController {
+
+
+    //To get data from UserService into UserDto
+    @Autowired
+    private UserService userService;
+
+
+    //Create API------------------------------------------------------------------------------------
+    @PostMapping
+    //     Status code | Return type        | To send data | To get Data
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+
+       UserDto userDto1 = userService.createUser(userDto);
+       //                             data  | Status code
+       return new ResponseEntity<>(userDto1, HttpStatus.CREATED);
+    }
+
+
+
+    //Update API-------------------------------------------------------------------------------------
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable String userId, @RequestBody UserDto userDto){
+
+       UserDto userDto2 = userService.updateUser(userDto, userId);
+
+       return new ResponseEntity<>(userDto2, HttpStatus.OK);
+
+    }
+
+
+    //Delete Api---------------------------------------------------------------------------------------
+    @DeleteMapping("/{userId}")
+    //                  Return Type
+    public ResponseEntity<String> deleteUser(@PathVariable String userId){
+
+            // Don't hold data for delete
+            userService.deleteUser(userId);
+
+            //Only pass return type in response as a String ""
+        return new ResponseEntity<>("User is deleted Successfully!!", HttpStatus.OK);
+    }
+
+
+
+    //Get All Api------------------------------------------------------------------------------
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUser(){
+
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+
+    }
+
+
+    //Get Single Api---------------------------------------------------------------------------------
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable String userId){
+
+        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK );
+
+    }
+
+
+    //Get by email Api---------------------------------------------------------------------------------
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email){
+
+        return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+    }
+
+
+    //Search user Api-----------------------------------------------------------------------------------
+
+    @GetMapping("/search/{keyword}")
+    public ResponseEntity<List<UserDto>> searchUser(@PathVariable String keyword){
+
+        return new ResponseEntity<>(userService.searchUser(keyword), HttpStatus.OK);
+    }
+
+
+
+
+
+}
