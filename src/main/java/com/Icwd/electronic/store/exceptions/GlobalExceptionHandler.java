4f739fdb7d06398fe617ad0ca12744bc2d7663e2
Abhishek Patel <abhishekpatel1238@gmail.com>
2024-02-26 17:47:04 +0530

    16) Handling Exception of validation API

--- src/main/java/com/Icwd/electronic/store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/Icwd/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -5,8 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,7 +21,7 @@ public class GlobalExceptionHandler {
     Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 
-    //Handler resource not found exception
+    // 1. Handler resource not found exception
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponseMessage> resourceNotFoundExceptionHandler(ResourceNotFoundException ex)
     {
@@ -29,7 +36,22 @@ public class GlobalExceptionHandler {
     }
 
 
+    // 2. MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
 
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String,Object> response = new HashMap<>();
+                                // Give one by one error
+        allErrors.stream().forEach(objectError -> {
+            String message = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+                      // Key    Value
+            response.put(field,message);
+        });
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
 
 }
