39f06dcad36a7201fd5c447e37073b0eb842e67b
Abhishek Patel <abhishekpatel1238@gmail.com>
2024-02-29 17:04:49 +0530

    26) Start Product Section | Creating Entity and DTO

---
+++ src/main/java/com/Icwd/electronic/store/dtos/ProductDto.java
@@ -0,0 +1,28 @@
+package com.Icwd.electronic.store.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductDto {
+
+    private String productId;
+    private String title;
+    private String description;
+    private int price;
+    private int discountedPrice;
+    private int quantity;
+    private Date addedDate;
+    private boolean live;
+    private boolean stock;
+
+}
